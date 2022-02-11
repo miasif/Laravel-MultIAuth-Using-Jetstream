c132c5cb017503a1c680d93bf38e1fda06f70a85
miasif <minhazul.islam.asif@gmail.com>
2022-02-11 21:54:49 +0600

    admin start

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,12 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        {{ Auth::user()->name }}
+
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        Dasdasdfas
+    </div>
+</x-app-layout>
